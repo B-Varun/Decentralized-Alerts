f31198a377ffbd88953c93d127ee9b236aed880e
B-Varun <bvarun662@gmail.com>
2023-11-23 15:20:36 -0800

    Added Main Contract

---
+++ pet-shop-tutorial/contracts/AlertManagement.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+//pragma solidity >=0.8.0 <=0.9.0;
+pragma solidity ^0.5.16;
+
+/*
+* AlertManagement is a smart contract that manages the alert system
+* This is the primary smart contract
+*/
+contract AlertManagement{
+
+}
